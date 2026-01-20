284b2fa05c465bbc9ac894d4c1d7eda81a413264
UnaiGuerra <guerramatasunai@gmail.com>
2026-01-20 12:54:33 +0100

    meta fix firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,11 +1,11 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import 'dotenv/config';
+import 'meta.env';
 
 const firebaseConfig = {
-    apiKey: process.env.apiKey,
-    authDomain: process.env.authDomain,
-    projectId: process.env.projectId,
+    apiKey: meta.env.apiKey,
+    authDomain: meta.env.authDomain,
+    projectId: meta.env.projectId,
     storageBucket: "rocketleague-b4f3b.firebasestorage.app",
     messagingSenderId: "991203471884",
     appId: "1:991203471884:web:ca4e57ddaf8792e007f429"
